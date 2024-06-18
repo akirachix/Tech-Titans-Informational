877c9dd2315068b69e1df2bdb6f8cb3d1016ca4b
rehema_funani <rehemafunani@gmail.com>
2024-06-18 15:36:09 +0300

    change the background colour

--- upcycleal/src/RoadMap/index.js
+++ upcycleal/src/RoadMap/index.js
@@ -9,15 +9,25 @@ const Roadmap = () =>{
         <div>
            
             <p className="Roadmap">
+                <div className ="Heading">
                 <h1>Road Map</h1>
+                </div>
                 <div className='Aim'>
                
                 <div className='Values'><h2>Values</h2><p>
+                <br/>
+            
+
+
                     <b>Sustainability:</b>We believe that Sustainability<br/>
                     is essential for the wellbeing of
                     our planet <br/>and its inhabitants
                     <br/>
                     <br/>
+                    <br/>
+                    
+
+
                     <p><b>Education:</b>We value education and believe <br/>that it is essential
                         for raising awareness<br/> about the importance of sustainability
                     </p>
@@ -25,10 +35,18 @@ const Roadmap = () =>{
                     </p></div>
                 <div className='Goals'>
                     <h2>Our Goals</h2>
+                    <br/>
+                    
+
+
                     
                     <p><b>Educate Users:</b>about the environmental impact <br/>of fast fashion and the benefits of upcycling</p>
                     <br/>
                     <br/>
+                    <br/>
+                    
+
+
                     <p><b>Reduce Waste:</b> Reduce clothe Waste
                     by promoting<br/> the upcycling  of 
                     discarded clothing into functional<br/>
