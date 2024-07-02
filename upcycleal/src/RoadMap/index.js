6b26bb241ec4ee249e6ba0303ac656e5927717b4
rehema_funani <rehemafunani@gmail.com>
2024-07-02 17:14:05 +0300

    adds roadmap

--- upcycleal/src/RoadMap/index.js
+++ upcycleal/src/RoadMap/index.js
@@ -8,46 +8,51 @@ const Roadmap = () =>{
     return(
         <div>
            
-            <p className="Roadmap">
+            <div className="Roadmap">
                 <div className ="Heading">
-                <h1>Road Map</h1>
+                <h1>ROAD MAP</h1>
                 </div>
                 <div className='Aim'>
                
                 <div className='Values'><h2>Values</h2><p>
-                <br/>
+                
             
 
 
-                    <b>Sustainability:</b>We believe that Sustainability<br/>
+                    Sustainability:We believe that sustainability<br/>
                     is essential for the wellbeing of
                     our planet <br/>and its inhabitants
                     <br/>
-                    <br/>
-                    <br/>
+                    
+                    
                     
 
 
-                    <p><b>Education:</b>We value education and believe <br/>that it is essential
+                    <p className="pp">Education:We value education and believe <br/>that it is essential
                         for raising awareness<br/> about the importance of sustainability
                     </p>
+                    {/* <img src = "./images/line2.png" className="line"/> */}
+
                     
-                    </p></div>
+                    </p>
+                    </div>
+                    
                 <div className='Goals'>
                     <h2>Our Goals</h2>
-                    <br/>
+                    
                     
 
 
                     
-                    <p><b>Educate Users:</b>about the environmental impact <br/>of fast fashion and the benefits of upcycling</p>
-                    <br/>
-                    <br/>
-                    <br/>
+                    <p>Educate Users:about the environmental impact <br/>of fast fashion and the benefits of upcycling
+                    <br/> in general</p>
+                    
+                    
+                    
                     
 
 
-                    <p><b>Reduce Waste:</b> Reduce clothe Waste
+                    <p>Reduce Waste: Reduce clothe waste
                     by promoting<br/> the upcycling  of 
                     discarded clothing into functional<br/>
                     products
@@ -55,7 +60,7 @@ const Roadmap = () =>{
                     </p>
                     </div>
                 </div>
-            </p>
+            </div>
             
         </div>
     )
